fdf5db39ffe5a2f9d87a6028e3d1159db2de017f
hayaduy <aduyphotograph@gmail.com>
2026-05-10 23:53:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 st.set_page_config(page_title="Absensi KPU HSS - V.1.5", layout="wide")
 
 # --- DATABASE & CONFIG ---
-URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbxr0weetaIjTrGZ6TmzWS4I_4VdUARM4NzeAqtdIC0G83Cf74nH3JyjpILSUltpJCAWig/exec"
+URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbyPd1XJ6-laTWMFOgsRhVYbccyEzxPh6TJUsZOi0pgGmzF6QNFRpzB32B2LxNPeYpVipg/exec"
 
 DB_PEGAWAI = {
     "1": {"nama": "Suwanto", "sheet": "Suwanto", "nip": "19720521 200912 1 001", "unit": "KPU KAB. HULU SUNGAI SELATAN"},
